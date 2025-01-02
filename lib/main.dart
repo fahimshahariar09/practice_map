72fa317b12954ee42e1da0c80dd4802ee9658993
Fahim Shahariar Shihab <fahimshahariar20002@gmail.com>
2025-01-01 23:46:13 -0800

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:practice/view/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,12 +8,10 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: HomeScreen(),
     );
   }
 }
