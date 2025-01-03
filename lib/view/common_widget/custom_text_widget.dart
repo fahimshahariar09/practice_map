c8074c44212f47e8f6dcc21b4f6915d49b1436b1
Fahim Shahariar Shihab <fahimshahariar20002@gmail.com>
2025-01-02 23:36:55 -0800

    first commit

--- lib/view/common_widget/custom_text_widget.dart
+++ lib/view/common_widget/custom_text_widget.dart
@@ -2,10 +2,26 @@ import 'package:flutter/material.dart';
 
 class CustomTextWidget extends StatelessWidget {
   const CustomTextWidget(
-      {super.key, required this.text, this.textLineThrough});
+      {super.key,
+      required this.text,
+      this.textLineThrough,
+      this.fontSize,
+      this.fontColor,
+      this.maxLine,
+      this.fontWeight,
+      this.textOverflow,
+      this.textAlign,
+      this.fontFamily});
 
   final String text;
   final bool? textLineThrough;
+  final double? fontSize;
+  final Color? fontColor;
+  final int? maxLine;
+  final FontWeight? fontWeight;
+  final TextOverflow? textOverflow;
+  final TextAlign? textAlign;
+  final String? fontFamily;
 
   @override
   Widget build(BuildContext context) {
