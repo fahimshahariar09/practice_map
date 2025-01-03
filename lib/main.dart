5d37f030b9fc83eba6c7fbcd727113d5e6da12a0
Fahim Shahariar Shihab <fahimshahariar20002@gmail.com>
2025-01-02 23:17:54 -0800

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:practice/view/home_screen.dart';
+import 'package:practice/view/screen/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
---
+++ lib/view/common_widget/custom_text_widget.dart
@@ -0,0 +1,8 @@
+class CustomTextWidget extends StatelessWidget {
+  const CustomTextWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
