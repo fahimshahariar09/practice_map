33d629a52a052a5805c39a16b0ea4a2149da5de0
Fahim Shahariar Shihab <fahimshahariar20002@gmail.com>
2025-01-02 23:57:00 -0800

    first commit

--- lib/view/common_widget/custom_button.dart
+++ lib/view/common_widget/custom_button.dart
@@ -1,17 +1,24 @@
 import 'package:flutter/material.dart';
 
 class CustomButton extends StatelessWidget {
-  const CustomButton({super.key});
+  const CustomButton({super.key, required this.text, required this.onTap});
+
+  final String text;
+  final VoidCallback onTap;
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: EdgeInsets.symmetric(vertical: 15, horizontal: 5),
-      color: Colors.white,
-      shape: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(5), borderSide: BorderSide.none),
-      child:
-          Padding(padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15)),
+    return InkWell(
+      onTap: onTap,
+      child: Card(
+        margin: EdgeInsets.symmetric(vertical: 15, horizontal: 5),
+        color: Colors.white,
+        shape: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(5),
+            borderSide: BorderSide.none),
+        child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15)),
+      ),
     );
   }
 }
