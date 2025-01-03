675d58c0957560d05b0207edb80e912c54efdd73
Fahim Shahariar Shihab <fahimshahariar20002@gmail.com>
2025-01-02 23:34:37 -0800

    first commit

--- lib/view/common_widget/custom_text_widget.dart
+++ lib/view/common_widget/custom_text_widget.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 
 class CustomTextWidget extends StatelessWidget {
-  const CustomTextWidget({super.key, required this.text});
+  const CustomTextWidget(
+      {super.key, required this.text, this.textLineThrough});
 
   final String text;
+  final bool? textLineThrough;
 
   @override
   Widget build(BuildContext context) {
@@ -11,13 +13,13 @@ class CustomTextWidget extends StatelessWidget {
       text,
       style: TextStyle(
           decorationStyle: TextDecorationStyle.solid,
+          decoration: TextDecoration.lineThrough,
           decorationThickness: 2.1,
           color: Colors.white,
           fontFamily: "RobotoSerif",
           fontWeight: FontWeight.w500,
           fontSize: 15,
-      overflow: TextOverflow.ellipsis
-      ),
+          overflow: TextOverflow.ellipsis),
     );
   }
 }
