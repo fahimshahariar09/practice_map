6510d58e36cdd37deac4062dcdefe8333f2a24e8
Fahim Shahariar Shihab <fahimshahariar20002@gmail.com>
2025-02-20 19:34:54 -0800

    first commit

--- lib/view/screen/aduio_screen.dart
+++ lib/view/screen/aduio_screen.dart
@@ -16,6 +16,7 @@ class AduioScreen extends StatelessWidget {
           Text("data"),
           Text("data"), Text("data"),
           Text("data"),
+          Text("data"),Text("data"),
           Text("data"),
           DraggableScrollableSheet(builder: (context, index) {
             return Container(
