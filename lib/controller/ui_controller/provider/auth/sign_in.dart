16756702df1f770a1e0ddb51659951dd8155204c
Fahim Shahariar Shihab <fahimshahariar20002@gmail.com>
2025-02-25 21:35:31 -0800

    first commit

--- lib/controller/ui_controller/provider/auth/sign_in.dart
+++ lib/controller/ui_controller/provider/auth/sign_in.dart
@@ -5,4 +5,6 @@ import 'package:flutter/foundation.dart';
 class SignInProvider  extends ChangeNotifier{
   bool isLoading = false;
   bool showPassword = true;
+  bool showConfirmPassword = true;
+
 }
