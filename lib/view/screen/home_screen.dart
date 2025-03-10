15465e162acd7c766923f64e19f1a38b4d5c6750
Fahim Shahariar Shihab <fahimshahariar20002@gmail.com>
2025-03-09 23:57:14 -0700

    first commit

--- lib/view/screen/home_screen.dart
+++ lib/view/screen/home_screen.dart
@@ -15,28 +15,8 @@ class HomeScreen extends StatelessWidget {
               fontSize: 15,
               fontWeight: FontWeight.w300,
             ),
-          ),Text(
-            "data",
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 15,
-              fontWeight: FontWeight.w300,
-            ),
-          ),Text(
-            "data",
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 15,
-              fontWeight: FontWeight.w300,
-            ),
-          ),Text(
-            "data",
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 15,
-              fontWeight: FontWeight.w300,
-            ),
           ),
+
         ],
       ),
     );
