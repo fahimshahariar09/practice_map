056a2342d7c0dff1c77460ac9457d19e61b5a1f2
Fahim Shahariar Shihab <fahimshahariar20002@gmail.com>
2025-03-11 23:00:13 -0700

    first commit

--- lib/view/screen/home_screen.dart
+++ lib/view/screen/home_screen.dart
@@ -8,8 +8,11 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       body: Column(
         children: [
-Text("data"),
-Text("data"),
+          Text("data"),
+          Text("data"), Text("data"),
+          Text("data"), Text("data"),
+          Text("data"), Text("data"),
+          Text("data"),
         ],
       ),
     );
